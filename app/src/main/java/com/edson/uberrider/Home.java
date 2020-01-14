e178ec30d2b60af1ee85500580975a08af531bc0
Edson <edyz86@hotmail.com>
2020-01-13 21:14:35 -0300

    Android Uber Clone - Part 6 : Rider App Integrated Location and Geo Fire.
    falta Implementar a pausa de requisição de de localização, que está comentada no OnCreate do Home activity

--- app/src/main/java/com/edson/uberrider/Home.java
+++ app/src/main/java/com/edson/uberrider/Home.java
@@ -1,26 +1,46 @@
 package com.edson.uberrider;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.github.glomadrian.materialanimatedswitch.MaterialAnimatedSwitch;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.os.Looper;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -35,6 +55,65 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     private AppBarConfiguration mAppBarConfiguration;
     SupportMapFragment mapFragment;
 
+    //Location
+    private GoogleMap mMap;
+    private static final int MY_PERMISSION_REQUEST_CODE = 7000;
+    private static final int PLAY_SERVICE_RES_REQUEST = 70001;
+    private static int UPDATE_INTERVAL = 5000;
+    private static int FATEST_INTERVAL = 3000;
+    private static int DISPLACEMENT = 10;
+    DatabaseReference ref;
+    GeoFire geoFire;
+    Marker mUserMarker;
+    MaterialAnimatedSwitch location_switch;
+
+    private LocationRequest mLocationRequest;
+    private Location mLastLocation;
+    LocationCallback locationCallback;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    /**
+     * Posting my solution as an answer in case it helps anyone else.
+     *
+     * I got it working by declaring a LocationCallback as a member variable and then initialising (or re-initialising) it in each method that requires it...
+     *
+     * public void getCurrentLocationUpdates(final UserLocationCallback callback){
+     *         if (mIsReceivingUpdates){
+     *             callback.onFailedRequest("Device is already receiving updates");
+     *             return;
+     *         }
+     *
+     *         // Set up the LocationCallback for the request
+     *         mLocationCallback = new LocationCallback()
+     *         {
+     *             @Override
+     *             public void onLocationResult(LocationResult locationResult){
+     *                 if (locationResult != null){
+     *                     callback.onLocationResult(locationResult.getLastLocation());
+     *                 } else {
+     *                     callback.onFailedRequest("Location request returned null");
+     *                 }
+     *             }
+     *         };
+     *
+     *         // Start the request
+     *         mLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+     *         // Update the request state flag
+     *         mIsReceivingUpdates = true;
+     *     }
+     * I check at the beginning of the method whether or not location updates are already being received and get out early if so. This prevents duplicate (and thus unstoppable) location update requests being initiated.
+     *
+     * Calling the stopLocationUpdates (below for reference) method now works as it should.
+     *
+     * public void stopLocationUpdates(){
+     *
+     *     mLocationClient.removeLocationUpdates(mLocationCallback);
+     *     mIsReceivingUpdates = false;
+     *     Log.i(TAG, "Location updates removed");
+     *
+     * }
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +138,134 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        buildLocationCallBack();
+        buildLocationRequest();
+        fusedLocationProviderClient.requestLocationUpdates(mLocationRequest, locationCallback, Looper.myLooper());
+        displayLocation();
+        Snackbar.make(mapFragment.getView(), "Você está Online!", Snackbar.LENGTH_SHORT)
+                .show();
+
+
+        //geofire
+        ref = FirebaseDatabase.getInstance().getReference("Drivers");
+        geoFire = new GeoFire(ref);
+
+        setUpLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+
+
+            case MY_PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    buildLocationCallBack();
+                    buildLocationRequest();
+                    displayLocation();
+                }
+
+        }
+
+    }
+
+    private void setUpLocation() {
+        //copiar do driver app
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                )) {
+            //request Runtime permission
+            ActivityCompat.requestPermissions(this, new String[]{
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSION_REQUEST_CODE);
+        } else {
+
+            buildLocationRequest();
+            buildLocationCallBack();
+            displayLocation();
+        }
+    }
+
+    private void buildLocationCallBack() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()) {
+                    mLastLocation = location;
+
+                }
+                displayLocation();
+            }
+        };
+    }
+
+    private void displayLocation() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                )) {
+
+            return;
+
+        }
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        mLastLocation = location;
+
+                        if (mLastLocation != null) {
+
+                            final double latitude = mLastLocation.getLatitude();
+                            final double longitude = mLastLocation.getLongitude();
+
+                            //update to firebase
+                            geoFire.setLocation(FirebaseAuth.getInstance().getCurrentUser().getUid(), new GeoLocation(latitude, longitude), new GeoFire.CompletionListener() {
+                                @Override
+                                public void onComplete(String key, DatabaseError error) {
+                                    //add marker
+                                    if (mUserMarker != null)
+                                        mUserMarker.remove(); //remove o marcador que ja esta
+                                    mUserMarker = mMap.addMarker(new MarkerOptions()
+                                            .position(new LatLng(latitude, longitude))
+                                            .title("Você"));
+
+
+                                    //mover a camera para essa posição
+                                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 15.0f));
+
+
+                                }
+                            });
+
+
+                        } else {
+
+                            Log.d("ERROR", "cannot get your location: ");
+                        }
+                    }
+                });
+
+
+    }
+
+    private void buildLocationRequest() {
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
     }
 
     @Override
@@ -78,11 +285,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        //add sample marker
-        googleMap.addMarker( new MarkerOptions()
-        .position(new LatLng(37.7750,-122.4183)).title("San Francisco"));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.7750,-122.4183), 12));
-
+        mMap = googleMap;
     }
 
     @Override
