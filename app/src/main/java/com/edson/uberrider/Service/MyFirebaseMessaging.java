d847ac01e3ce7848a5551c3a870140a5a1f3d475
EdsonGomesJR <edyz86@hotmail.com>
2020-02-04 16:40:42 -0300

    Aula 13 - Metodo para receber a notificação que o motorista chegou ao local

--- app/src/main/java/com/edson/uberrider/Service/MyFirebaseMessaging.java
+++ app/src/main/java/com/edson/uberrider/Service/MyFirebaseMessaging.java
@@ -1,35 +1,101 @@
 package com.edson.uberrider.Service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
+import com.edson.uberrider.R;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.google.gson.Gson;
+
+import java.util.Random;
 
 public class MyFirebaseMessaging extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(final RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        //Because this is outside of main thread, so if want to  run Toast, you'll need to create a Handler to do that
-        //choose Handler from android.os
+
+        if (remoteMessage.getNotification().getTitle().equals("Cancel")) {
+
+            //Because this is outside of main thread, so if want to  run Toast, you'll need to create a Handler to do that
+            //choose Handler from android.os
 
 
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
 
-                Toast.makeText(MyFirebaseMessaging.this, ""+remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MyFirebaseMessaging.this, "" + remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else if (remoteMessage.getNotification().getTitle().equals("Arrived")) {
+
+                showArrivedNotification(remoteMessage.getNotification().getBody());
+                }
             }
-        });
 
+    private void showArrivedNotification(String body) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "com.edson.uberrider.Service.test";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("EDS Channel");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Arrived")
+                .setContentText(body)
+                .setContentInfo("Info");
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
     }
+
+    /**  private void showArrivedNotification(String body) {
+
+        //this method only works for api 25 and lower, for api 26+ i'll have to use Notification Channel
+
+        PendingIntent contentIntent = PendingIntent.getActivity(getBaseContext(),
+                0,new Intent(), PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder builder =  new NotificationCompat.Builder(getBaseContext());
+
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Arrived")
+                .setContentText(body)
+                .setContentIntent(contentIntent);
+        NotificationManager manager = (NotificationManager) getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1,builder.build());
+    }*/
 }
+
